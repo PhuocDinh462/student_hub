5934de9074a8b053cb78efd3807da0fc876f6cdc
Bao <20127557@student.hcmus.edu.vn>
2024-03-23 22:54:15 +0700

    fix: update navigator to chat screen

--- lib/screens/chat/widgets/message_item.widget.dart
+++ lib/screens/chat/widgets/message_item.widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:student_hub/routes/app_routes.dart';
 import 'package:student_hub/utils/utils.dart';
 import 'package:student_hub/widgets/widgets.dart';
 
@@ -11,87 +12,92 @@ class MessageItem extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
     final deviceSize = context.deviceSize;
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Row(
-        children: [
-          Container(
-            width: 55,
-            height: 55,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              image: DecorationImage(
-                image: AssetImage('assets/images/avatar.jpg'),
-                fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).pushNamed(AppRoutes.chatScreen);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: Row(
+          children: [
+            Container(
+              width: 55,
+              height: 55,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage('assets/images/avatar.jpg'),
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(1000),
-                    ),
-                    child:
-                        //  Container(
-                        //   margin: const EdgeInsets.only(right: 3),
-                        //   child: const Icon(
-                        //     Icons.circle,
-                        //     size: 14,
-                        //     color: Colors.green,
-                        //   ),
-                        // ),
-                        Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 4),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(1000),
-                          color: Colors.green.withOpacity(0.2)),
-                      child: DisplayText(
-                        text: '12p',
-                        style: textTheme.labelLarge!.copyWith(fontSize: 8),
-                      ),
-                    )),
-              ],
-            ),
-          ),
-          const Gap(5),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              DisplayText(
-                text: 'User 2',
-                style: textTheme.labelMedium!,
-              ),
-              DisplayText(
-                  text: 'Senior frontend developer (Fintech)',
-                  style: textTheme.labelSmall!.copyWith(
-                    fontSize: 10,
-                    color: colorScheme.onSurface,
-                  )),
-              const Gap(5),
-              Row(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  SizedBox(
-                    width: deviceSize.width * 0.6,
-                    child: DisplayText(
-                      text:
-                          'Clear expectation about your project or deliverables',
+                  Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(1000),
+                      ),
+                      child:
+                          //  Container(
+                          //   margin: const EdgeInsets.only(right: 3),
+                          //   child: const Icon(
+                          //     Icons.circle,
+                          //     size: 14,
+                          //     color: Colors.green,
+                          //   ),
+                          // ),
+                          Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 4),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(1000),
+                            color: Colors.green.withOpacity(0.2)),
+                        child: DisplayText(
+                          text: '12p',
+                          style: textTheme.labelLarge!.copyWith(fontSize: 8),
+                        ),
+                      )),
+                ],
+              ),
+            ),
+            const Gap(5),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                DisplayText(
+                  text: 'User 2',
+                  style: textTheme.labelMedium!,
+                ),
+                DisplayText(
+                    text: 'Senior frontend developer (Fintech)',
+                    style: textTheme.labelSmall!.copyWith(
+                      fontSize: 10,
+                      color: colorScheme.onSurface,
+                    )),
+                const Gap(5),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: deviceSize.width * 0.6,
+                      child: DisplayText(
+                        text:
+                            'Clear expectation about your project or deliverables',
+                        style: textTheme.labelSmall!
+                            .copyWith(color: colorScheme.onSurface),
+                      ),
+                    ),
+                    DisplayText(
+                      text: '· 00:00',
                       style: textTheme.labelSmall!
                           .copyWith(color: colorScheme.onSurface),
                     ),
-                  ),
-                  DisplayText(
-                    text: '· 00:00',
-                    style: textTheme.labelSmall!
-                        .copyWith(color: colorScheme.onSurface),
-                  ),
-                ],
-              )
-            ],
-          )
-        ],
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
