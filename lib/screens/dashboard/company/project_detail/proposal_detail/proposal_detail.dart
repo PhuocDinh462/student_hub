795a7b92da2e41c70fc984ab96c34d23b7abf106
Phuoc Dinh <phuocdinh462@gmail.com>
2024-04-19 00:53:58 +0700

    refactor: Refactor skill_item.dart widget and update skill size

--- lib/screens/dashboard/company/project_detail/proposal_detail/proposal_detail.dart
+++ lib/screens/dashboard/company/project_detail/proposal_detail/proposal_detail.dart
@@ -12,125 +12,322 @@ class ProposalDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     // final int proposalId = Get.arguments;
 
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: SizedBox(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Profile
-            Row(
-              children: [
-                Image.asset(
-                  'assets/images/user.png',
-                  width: 80,
-                ),
-                const Gap(20),
-                SizedBox(
-                  width: context.deviceSize.width - 150,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Phuoc Dinh Cao Hong',
-                        style: Theme.of(context).textTheme.displayLarge,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SizedBox(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Profile
+              Row(
+                children: [
+                  Image.asset(
+                    'assets/images/user.png',
+                    width: 80,
+                  ),
+                  const Gap(20),
+                  SizedBox(
+                    width: context.deviceSize.width - 150,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Phuoc Dinh Cao Hong',
+                          style: Theme.of(context)
+                              .textTheme
+                              .displayLarge
+                              ?.copyWith(
+                                color: Theme.of(context).colorScheme.primary,
+                                fontWeight: FontWeight.w600,
+                              ),
+                        ),
+                        const Gap(5),
+                        Text(
+                          'Fullstack Engineer',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodySmall
+                              ?.copyWith(fontStyle: FontStyle.italic),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const Gap(15),
+
+              // Cover letter
+              SizedBox(
+                width: context.deviceSize.width - 30,
+                child: const Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'),
+              ),
+              const Gap(30),
+
+              // Educations
+              Row(
+                children: [
+                  Text(
+                    'Educations',
+                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontStyle: FontStyle.italic),
+                  ),
+                  const Expanded(
+                    child: Divider(
+                      height: 40,
+                      thickness: .5,
+                      indent: 30,
+                      color: text_400,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('University of Science - VNUHCM'),
+                  Text(
+                    '2020 - 2024',
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelSmall
+                        ?.copyWith(fontStyle: FontStyle.italic),
+                  ),
+                  const Gap(15),
+                  const Text('University of Science - VNUHCM'),
+                  Text(
+                    '2020 - 2024',
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelSmall
+                        ?.copyWith(fontStyle: FontStyle.italic),
+                  ),
+                ],
+              ),
+              const Gap(30),
+
+              // Skills
+              Row(
+                children: [
+                  Text(
+                    'Skills',
+                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontStyle: FontStyle.italic),
+                  ),
+                  const Expanded(
+                    child: Divider(
+                      height: 40,
+                      thickness: .5,
+                      indent: 30,
+                      color: text_400,
+                    ),
+                  ),
+                ],
+              ),
+              const Gap(5),
+              const Wrap(
+                spacing: 10,
+                runSpacing: 10,
+                children: [
+                  SkillItem(skill: 'React'),
+                  SkillItem(skill: 'Javascript'),
+                  SkillItem(skill: 'NodeJS'),
+                  SkillItem(skill: 'CI/CD'),
+                  SkillItem(skill: 'Flutter'),
+                ],
+              ),
+              const Gap(30),
+
+              // Resume & Transcript
+              Row(
+                children: [
+                  Text(
+                    'Resume & Transcript',
+                    style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontStyle: FontStyle.italic),
+                  ),
+                  const Expanded(
+                    child: Divider(
+                      height: 40,
+                      thickness: .5,
+                      indent: 30,
+                      color: text_400,
+                    ),
+                  ),
+                ],
+              ),
+              // Resume
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: RichText(
+                      text: TextSpan(children: [
+                        TextSpan(
+                            text: 'Resume: ',
+                            style: Theme.of(context).textTheme.bodyMedium),
+                        TextSpan(
+                            text: '1713367523615-Mock CV.pdf',
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelSmall
+                                ?.copyWith(fontStyle: FontStyle.italic)),
+                      ]),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.open_in_browser, size: 30),
+                  ),
+                ],
+              ),
+              // Transcript
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: RichText(
+                      text: TextSpan(children: [
+                        TextSpan(
+                            text: 'Transcript: ',
+                            style: Theme.of(context).textTheme.bodyMedium),
+                        TextSpan(
+                            text: '1713367527862-logo.png',
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelSmall
+                                ?.copyWith(fontStyle: FontStyle.italic)),
+                      ]),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.open_in_browser, size: 30),
+                  ),
+                ],
+              ),
+              const Gap(40),
+
+              // Action buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: primary_300,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      const Gap(5),
-                      Text(
-                        'Fullstack Engineer',
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodySmall
-                            ?.copyWith(fontStyle: FontStyle.italic),
+                    ),
+                    child: const SizedBox(
+                      width: 100,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(Icons.message_outlined,
+                              color: text_50, size: 20),
+                          Gap(10),
+                          Text(
+                            'Message',
+                            style: TextStyle(
+                              color: text_50,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
+                    ),
                   ),
-                )
-              ],
-            ),
-            const Gap(15),
-
-            // Cover letter
-            SizedBox(
-              width: context.deviceSize.width - 30,
-              child: const Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'),
-            ),
-            const Gap(30),
-
-            // Educations
-            Row(
-              children: [
-                Text(
-                  'Educations',
-                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontStyle: FontStyle.italic),
-                ),
-                const Expanded(
-                  child: Divider(
-                    height: 40,
-                    thickness: .5,
-                    indent: 30,
-                    color: text_400,
+                  const Gap(30),
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text(
+                              'Hired offer',
+                              style: Theme.of(context).textTheme.displayMedium,
+                            ),
+                            content: const Text(
+                                'Do you really want to send hired offer for student to do this project?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  // Cancel hire action
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  'Cancel',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleMedium
+                                      ?.copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onSurface
+                                            .withOpacity(.75),
+                                      ),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  // Perform hire action
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text(
+                                  'Send',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleMedium
+                                      ?.copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .primary,
+                                      ),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: primary_300,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: const SizedBox(
+                      width: 100,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(Icons.check_box_outlined,
+                              color: text_50, size: 20),
+                          Gap(10),
+                          Text(
+                            'Hire',
+                            style: TextStyle(
+                              color: text_50,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text('University of Science - VNUHCM'),
-                Text(
-                  '2020 - 2024',
-                  style: Theme.of(context)
-                      .textTheme
-                      .labelSmall
-                      ?.copyWith(fontStyle: FontStyle.italic),
-                ),
-                const Gap(15),
-                const Text('University of Science - VNUHCM'),
-                Text(
-                  '2020 - 2024',
-                  style: Theme.of(context)
-                      .textTheme
-                      .labelSmall
-                      ?.copyWith(fontStyle: FontStyle.italic),
-                ),
-              ],
-            ),
-            const Gap(30),
-
-            // Skills
-            Row(
-              children: [
-                Text(
-                  'Skills',
-                  style: Theme.of(context).textTheme.headlineLarge?.copyWith(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontStyle: FontStyle.italic),
-                ),
-                const Expanded(
-                  child: Divider(
-                    height: 40,
-                    thickness: .5,
-                    indent: 30,
-                    color: text_400,
-                  ),
-                ),
-              ],
-            ),
-            const Wrap(
-              spacing: 10,
-              runSpacing: 10,
-              children: [
-                Tooltip(
-                  message: 'React',
-                  child: SkillItem(skill: 'React'),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/dashboard/company/project_detail/proposal_detail/widgets/skill_item.dart
+++ lib/screens/dashboard/company/project_detail/proposal_detail/widgets/skill_item.dart
@@ -4,16 +4,19 @@ import 'package:student_hub/constants/skills.dart';
 class SkillItem extends StatelessWidget {
   const SkillItem({super.key, required this.skill});
   final String skill;
-  final double size = 60;
+  final double size = 50;
 
   @override
   Widget build(BuildContext context) {
     String skillUrl = skills[skill] ?? '';
-    return skillUrl.isNotEmpty
-        ? Image.asset(
-            'assets/images/skills/${Theme.of(context).brightness == Brightness.light ? 'light' : 'dark'}/$skillUrl',
-            width: size,
-            height: size)
-        : Text(skill);
+    return Tooltip(
+      message: skill,
+      child: skillUrl.isNotEmpty
+          ? Image.asset(
+              'assets/images/skills/${Theme.of(context).brightness == Brightness.light ? 'light' : 'dark'}/$skillUrl',
+              width: size,
+              height: size)
+          : Text(skill),
+    );
   }
 }
