b90fee4ade3379592d0a8c8b14ceb7b60d3ca4de
Phuoc Dinh <phuocdinh462@gmail.com>
2024-05-10 21:40:23 +0700

    chore: Remove unused import in alert screen

--- lib/screens/alerts/alert.screen.dart
+++ lib/screens/alerts/alert.screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
 import 'package:student_hub/models/models.dart';
 import 'package:student_hub/providers/providers.dart';
