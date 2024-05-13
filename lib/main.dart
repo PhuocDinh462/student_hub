1e923d60118de06cdd3c5f3fbb2df8313a8146a7
Phuoc Dinh <phuocdinh462@gmail.com>
2024-05-14 05:04:17 +0700

    feat: Update main screen loading color and image handling

--- lib/main.dart
+++ lib/main.dart
@@ -87,13 +87,11 @@ class MyApp extends StatelessWidget {
     return FutureBuilder(
       future: initializeProviders(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        final ThemeProvider themeProvider = Provider.of<ThemeProvider>(context);
-
         if (snapshot.connectionState == ConnectionState.waiting) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
             home: Container(
-              color: themeProvider.getThemeMode ? text_900 : Colors.white,
+              color: Theme.of(context).colorScheme.onPrimary,
               child: Center(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -134,6 +132,8 @@ class MyApp extends StatelessWidget {
               textDirection: TextDirection.ltr,
             );
           } else {
+            final ThemeProvider themeProvider =
+                Provider.of<ThemeProvider>(context);
             final ProjectProvider projectProvider =
                 Provider.of<ProjectProvider>(context);
             Get.put(projectProvider);
